12d5baf19e71a17523b8be6ee1cc9783294fa0c7
Aniket Srivastava <ANIKET.SRIVASTAVA.ID@GMAIL.COM>
2025-02-01 20:23:25 +0530

    L 2 completed

---
+++ Server/controller/Course.js
@@ -0,0 +1,12 @@
+const Course = require('../models/Course');
+const Tag = require('../models/Tag');
+const User = require('../models/User');
+const {uploadImageToCloudinary} = require('../utils/imageUploader');
+
+exports.createCourse = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
